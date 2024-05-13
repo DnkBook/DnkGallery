96186b4a908885756282ac69e846af0dc814b112
xueque <maqueyuxue@outlook.com>
2024-05-14 01:13:03 +0800

    fix: navigation view

--- DnkGallery.Presentation/Pages/MainPage.cs
+++ DnkGallery.Presentation/Pages/MainPage.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.UI.Xaml;
 using DataTemplate = CSharpMarkup.WinUI.DataTemplate;
 
 namespace DnkGallery.Presentation.Pages;
 
-partial class MainPage{
-    private Grid AppTitleBar(string title) => Grid(
-            Columns(Auto, Auto),
+partial class MainPage {
+    private Grid AppTitleBar(string title) => Grid(Columns(Auto, Auto),
             Image().Width(16).Height(16)
                 .HorizontalAlignment(HorizontalAlignment.Left)
                 .VCenter(),
@@ -13,27 +13,26 @@ partial class MainPage{
                 .Margin(12, 0, 0, 0)
                 .Grid_Column(1)
                 .Text(title)
-                .VCenter().Margin(28, 0, 0, 0)
-        )
+                .VCenter().Margin(28, 0, 0, 0))
         .Height(48)
         .Margin(48, 0, 0, 0)
         .VerticalAlignment(VerticalAlignment.Top)
         .Padding(0);
     
-    public void BuildUI() => Content(
-        Grid(
-            AppTitleBar("DnkGallery"),
-            NavigationView(
-                    Frame()
-                ).MenuItemsSource().Bind(vm.NavigationViewItems)
+    public void BuildUI() => Content(Grid(AppTitleBar("DnkGallery"),
+        NavigationView(
+                Frame().Invoke(frame=> vm.Frame = frame)
+                )
+            .MenuItemsSource()
+            .Bind(vm?.NavigationViewItems)
             .MenuItemTemplate(MenuItemTemplate)
+            
         )
     );
     
-
     
-    private DataTemplate MenuItemTemplate => DataTemplate(() => HStack(
-        FontIcon().Glyph("\uE787"),
-        TextBlock().Text().Bind()
-    ).Spacing(24));
+    
+    private DataTemplate MenuItemTemplate => DataTemplate(
+        () => NavigationViewItem().Content().Bind("Name").Icon().Bind("Icon").MenuItemsSource().Bind("Children")
+        );
 }
--- DnkGallery.Presentation/Pages/MainPage.logic.cs
+++ DnkGallery.Presentation/Pages/MainPage.logic.cs
@@ -2,9 +2,7 @@
 
 [UIBindable]
 public sealed partial class MainPage : BasePage<BindableMainViewModel>, IBuildUI {
-    public MainPage() {
-        BuildUI();
-    }
+    public MainPage() => BuildUI();
     
     
 }
@@ -16,10 +14,51 @@ public partial record MainViewModel : BaseViewModel {
         await SetState(Text, _ => value.text);
     }
     
-    public IState<ObservableCollection<dynamic>> NavigationViewItems => UseState(() => categories);
+    public IState<ObservableCollection<Category>> NavigationViewItems => UseState(() => Categories);
     
-    private readonly ObservableCollection<dynamic> categories = [
-         "Menu Item 1",
-         "Menu Item 2",
+    public Frame? Frame;
+    
+    private readonly ObservableCollection<Category> Categories = [
+        new Category() {
+            Name = "Menu Item 1",
+            Icon = Icon("Calender"),
+            Children = new ObservableCollection<Category>() {
+                new Category() {
+                    Name = "Menu Item 2",
+                    Icon = Icon("Calender"),
+                    Children = new ObservableCollection<Category>() {
+                        new Category() {
+                            Name = "Menu Item 2",
+                            Icon = Icon("Calender"),
+                            Children = new ObservableCollection<Category>() {
+                                new Category() { Name = "Menu Item 3", Icon = Icon("Calender"), },
+                                new Category() { Name = "Menu Item 4", Icon = Icon("Calender"), }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        new Category() {
+            Name = "Menu Item 5",
+            Icon = Icon("Calender"),
+            Children = new ObservableCollection<Category>() {
+                new Category() {
+                    Name = "Menu Item 6",
+                    Icon = Icon("Calender"),
+                    Children = new ObservableCollection<Category>() {
+                        new Category() { Name = "Menu Item 7", Icon = Icon("Calender"), },
+                        new Category() { Name = "Menu Item 8", Icon = Icon("Calender"), }
+                    }
+                }
+            }
+        },
+        new Category() { Name = "Menu Item 9", Icon = Icon("Calender"), }
     ];
+}
+
+public class Category {
+    public string Name { get; set; }
+    public IconElement Icon { get; set; }
+    public ObservableCollection<Category> Children { get; set; }
 }
