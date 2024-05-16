c7ca13b893036354b381f043f2de35683dbe5aee
xueque <maqueyuxue@outlook.com>
2024-05-16 23:30:33 +0800

    style: setting expander

--- DnkGallery.Presentation/Core/MarkupExtensions.cs
+++ DnkGallery.Presentation/Core/MarkupExtensions.cs
@@ -3,11 +3,11 @@ using UIElement = Microsoft.UI.Xaml.UIElement;
 
 namespace DnkGallery.Presentation.Core;
 
-static class MarkupExtensions {
+internal static class MarkupExtensions {
     // Add any application-specific Markup extensions here
 }
 
-static class MarkupHelpers {
+internal static class MarkupHelpers {
     internal static BitmapIcon Icon(string appAsset) => BitmapIcon(UriSource: new Uri($"ms-appx:///Assets/{appAsset}.png"));
     
     internal static Image Image(string appAsset) => CSharpMarkup.WinUI.Helpers.Image(Source: BitmapImage(new Uri($"ms-appx:///Assets/{appAsset}.png")));
@@ -15,32 +15,39 @@ static class MarkupHelpers {
     internal static TextBlock ExampleFooter() => TextBlock(Span("Built with C# Markup "), Span("2").FontSize(18), Span(" for Uno")).FontStyle().Italic()
         .Bottom().HCenter();
     
+    public static UIElement SettingsExpanderContent(UIElement left, UIElement? right = null) {
+        return Grid(
+                Columns(Auto,Star),
+                HStack(left).VCenter().Margin(0,0,16,0),
+            right is not null ? HStack(right)
+                .HorizontalAlignment(HorizontalAlignment.Right)
+                .VCenter()
+                .Grid_Column(1)
+                : null)
+            .HorizontalAlignment(HorizontalAlignment.Stretch);
+    }
     
-    public static Expander SettingsExpander(CSharpMarkup.WinUI.UIElement content,
+    public static Expander SettingsExpander(UIElement[] content,
         UIElement icon,
         string title,
         string description,
-        UIElement right) {
+        UIElement? right = null) {
         return Expander(
-            content
-        ).Header(
-            Grid(
-                HStack(
-                    icon,
-                    VStack(
-                        TextBlock(title),
-                        TextBlock(description).FontSize(12).Foreground(ThemeResource.TextFillColorSecondaryBrush)
-                        ).VCenter()
-                ).Spacing(24)
-                .HorizontalAlignment(HorizontalAlignment.Left)
-                .VCenter(),
-                Grid(right)
+            VStack(content)
+                .Margin(44, 0, 44, 0)
+                .Spacing(16)
+            ).Header(
+                Grid(
+                    HStack(icon,
+                        VStack(TextBlock(title),
+                            TextBlock(description).FontSize(12).Foreground(ThemeResource.TextFillColorSecondaryBrush)).VCenter()).Spacing(24)
+                    .HorizontalAlignment(HorizontalAlignment.Left)
+                    .VCenter(),
+                right is not null ? Grid(right)
                     .HorizontalAlignment(HorizontalAlignment.Right)
-                    .VCenter()
-            ).Height(64)
+                    .VCenter() : null).Padding(0, 16, 0, 16)
             .HorizontalAlignment(HorizontalAlignment.Stretch)
-                .VCenter().UI
+            .VCenter().UI
         ).HorizontalAlignment(HorizontalAlignment.Stretch).HorizontalContentAlignment(HorizontalAlignment.Stretch);
     }
-    // Add more application-specific Markup helpers here
 }
--- DnkGallery.Presentation/Pages/SettingPage.cs
+++ DnkGallery.Presentation/Pages/SettingPage.cs
@@ -1,38 +1,36 @@
-using CSharpMarkup.WinUI.Uno.Toolkit;
 using DnkGallery.Model;
 using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
+using UIElement = Microsoft.UI.Xaml.UIElement;
 
 namespace DnkGallery.Presentation.Pages;
 
 public partial class SettingPage {
     public void BuildUI() => Content(
         VStack(
-            TextBlock("基本设置").FontWeight(FontWeights.Bold),
-            VStack (
-                
-                SettingsExpander(
-                    HStack(
-                        TextBlock("源地址").VCenter(),
-                        TextBox()
-                            .Text().Bind(vm?.Setting?.SourcePath, BindingMode.TwoWay)
-                            .HorizontalAlignment(HorizontalAlignment.Left).MaxWidth(500)
-                    ).Spacing(24),
-                    SymbolIcon(UIControls.Symbol.Folder),
-                    "语录册源",
-                    "使用本地源或者从Git上获取",
-                    ComboBox()
-                        .ItemsSource(Enum.GetValues(typeof(Source)))
-                        .SelectedItem()
-                        .Bind(vm?.Setting?.Source, BindingMode.TwoWay)
-                        .VCenter()
-                )
-                // ,
-                // HStack(
-                //     Button("保存").BindCommand(vm?.Save),
-                //     Button("取消").BindCommand(vm?.Cancel)
-                // ).Spacing(24)
-            ).Spacing(2).HorizontalAlignment(HorizontalAlignment.Stretch)
-            ).Margin(24).Spacing(12)
+            TextBlock("基本设置")
+                .FontWeight(FontWeights.Bold),
+             VStack(
+                 BaiscSettingItems()
+             ).Spacing(2).HorizontalAlignment(HorizontalAlignment.Stretch)
+            
+        ).Margin(24).Spacing(12)
     );
+    
+    private UIElement[] BaiscSettingItems() => [
+        SettingsExpander([
+                SettingsExpanderContent(TextBlock("源类型"), 
+                    ComboBox()
+                    .ItemsSource(Enum.GetValues(typeof(Source)))
+                    .Width(300)
+                    .SelectedItem()
+                    .Bind(vm?.Setting?.Source, BindingMode.TwoWay)),
+                SettingsExpanderContent(TextBlock("源地址"), 
+                    TextBox()
+                        .MaxWidth(300)
+                    .Text().Bind(vm?.Setting?.SourcePath, BindingMode.TwoWay))
+            ], SymbolIcon(UIControls.Symbol.Folder),
+            "语录册源",
+            "使用本地源或者从Git上获取")
+    ];
 }
