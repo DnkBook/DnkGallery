a0bb70276dbafa3ce6f2ec17f0dda403044db4dd
xueque <maqueyuxue@outlook.com>
2024-05-16 23:36:03 +0800

    fix: fix clipboard multi invoke

--- DnkGallery.Presentation/Pages/GalleryPage.logic.cs
+++ DnkGallery.Presentation/Pages/GalleryPage.logic.cs
@@ -7,6 +7,7 @@ using Windows.System;
 using DnkGallery.Model;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Uno.Extensions;
 using BitmapImage = Microsoft.UI.Xaml.Media.Imaging.BitmapImage;
@@ -27,11 +28,13 @@ public sealed partial class GalleryPage : BasePage<BindableGalleryViewModel>, IB
     
     private void ContentInvoke(UIControls.Page obj) {
         obj.Loaded += (sender, args) => {
-            itemsView.ItemTemplate = ItemViewTemplate;
+            // itemsView.ItemTemplate = ItemViewTemplate;
             RegisterAccelerator();
         };
     }
-    
+    /// <summary>
+    /// 订阅快捷键事件
+    /// </summary>
     private void RegisterAccelerator() {
         var copyAccelerator = new KeyboardAccelerator {
             Key = VirtualKey.C,
@@ -52,11 +55,14 @@ public sealed partial class GalleryPage : BasePage<BindableGalleryViewModel>, IB
     protected override async void OnNavigatedTo(NavigationEventArgs e) {
         var chapter = e.Parameter as Chapter;
         await vm.Model.Chapter.Update(_ => chapter, CancellationToken.None);
-        vm.Model.LoadAnas();
-        
+        await vm.Model.LoadAnas();
         base.OnNavigatedTo(e);
     }
-    
+    /// <summary>
+    /// Ctrl + C复制
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     private async void CopyInvoke(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args) {
         if (itemsView.SelectedItem is not Ana itemsViewSelectedItem)
             return;
@@ -67,18 +73,33 @@ public sealed partial class GalleryPage : BasePage<BindableGalleryViewModel>, IB
         dataPackage.SetBitmap(randomAccessStreamReference);
         Clipboard.SetContent(dataPackage);
     }
-    
+    /// <summary>
+    /// Ctrl + V粘贴，Win + V可选调出粘贴板
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
     private async void PasteInvoke(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args) {
-        var dataPackageView = Clipboard.GetContent();
-        if (!dataPackageView.Contains(StandardDataFormats.Bitmap)) {
+        // 标记此事件已被处理
+        // 有时事件会触发两次 dialog打开两次导致异常，不明所以
+        args.Handled = true;
+        // 判断当前有无dialog打开
+        if (VisualTreeHelper.GetOpenPopups(MainWindow).Count > 0)
             return;
-        }
+        
+        var dataPackageView = Clipboard.GetContent();
+        //判断是否是图片
+        if (!dataPackageView.Contains(StandardDataFormats.Bitmap))
+            return;
         
         var randomAccessStreamReference = await dataPackageView.GetBitmapAsync();
-        var randomAccessStreamWithContentType = await randomAccessStreamReference.OpenReadAsync();
+        using var randomAccessStreamWithContentType = await randomAccessStreamReference.OpenReadAsync();
+        
+        // 这里保存到BitmapImage用于显示
         var bitmapImage = new BitmapImage();
         await bitmapImage.SetSourceAsync(randomAccessStreamWithContentType);
+        // 设置到初始位置再读一遍
         randomAccessStreamWithContentType.Seek(0);
+        // 保存到WriteableBitmap获得流后面用于保存
         var writeableBitmap = new WriteableBitmap(bitmapImage.PixelWidth, bitmapImage.PixelHeight);
         await writeableBitmap.SetSourceAsync(randomAccessStreamWithContentType);
         var stream = writeableBitmap.PixelBuffer.AsStream();
@@ -90,7 +111,7 @@ public sealed partial class GalleryPage : BasePage<BindableGalleryViewModel>, IB
             PixelWidth = bitmapImage.PixelWidth,
             PixelHeight = bitmapImage.PixelHeight
         };
-        vm.Model.SaveData.Update(_ => saveAnaData, CancellationToken.None);
+        await vm.Model.SaveData.Update(_ => saveAnaData, CancellationToken.None);
         await saveDialog.ShowAsync();
     }
 }
@@ -117,7 +138,6 @@ public partial record GalleryViewModel : BaseViewModel {
         var saveFileName = saveAnaData.FileName;
         await using var pixelStream = saveAnaData.Stream;
         
-        var path = Path.Combine(chapter.Dir, saveFileName);
         var folderFromPath = await StorageFolder.GetFolderFromPathAsync(chapter.Dir);
         var storageFile = await folderFromPath.CreateFileAsync(saveFileName);
         using var randomAccessStream =
@@ -137,6 +157,7 @@ public partial record GalleryViewModel : BaseViewModel {
             pixels);
         await bitmapEncoder.FlushAsync();
         
+        // 保存完reload一下
         await LoadAnas();
     }
     
